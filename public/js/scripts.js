f13735bae1135714bc2f5fc7efe3fa1b6913aba1
Palash <palashshah345@gmail.com>
2023-03-27 12:26:50 +1100

    Practical 3 pushed to github

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,9 +1,9 @@
 const cardList = [
     {
-        title: 'Kitten 2', image: 'images/kitten-2.png', link: 'About Kitten 2', description: 'Kitten 2 description'
+        title: 'Dog 2', image: 'images/Dog-2.png', link: 'About Dog2', description: 'Dog 2 description'
     },
     {
-        title: 'Kitten 3', image: 'images/kitten-3.png', link: 'About Kitten 3', description: 'Kitten 3 description'
+        title: 'Dog 3', image: 'images/Dog-3.png', link: 'About Dog 3', description: 'Dog 3 description'
     }
 ];
 
@@ -14,7 +14,7 @@ const clickMe = () => {
 const addCards = (items) => {
     console.log(items);
     items.forEach(item => {
-        let itemToAppend = '<div class="col s4 center-align"> <div class="card medium"><div class="card-image waves-effect waves-block waves-light"><img class="activator" src="'+ item.image +'"></img></div><div class="card-content"><span class="card-title activator grey-text text-darken-4">'+ item.title +'<i class="material-icons right">more_vert</i></span><p><a href="#">About this kiiten</a></p></div><div class="card-reveal"><span class="card-title grey-text text-darken-4">'+item.title+'<i class="material-icons right">close</i></span><p class="card-text">'+item.description+'</p></div></div></div>';
+        let itemToAppend = '<div class="col s4 center-align"> <div class="card medium"><div class="card-image waves-effect waves-block waves-light"><img class="activator" src="'+ item.image +'"></img></div><div class="card-content"><span class="card-title activator grey-text text-darken-4">'+ item.title +'<i class="material-icons right">more_vert</i></span><p><a href="#">'+item.link+'</a></p></div><div class="card-reveal"><span class="card-title grey-text text-darken-4">'+item.title+'<i class="material-icons right">close</i></span><p class="card-text">'+item.description+'</p></div></div></div>';
         $('#card-section').append(itemToAppend);
     });
 }
